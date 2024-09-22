ab2bade8e1caa214d051ade0494cd53516573f49
EekusGD <andrewche321@gmail.com>
2024-09-22 16:19:15 +0300

    Add files via upload
    lol guess it

---
+++ lol.py
@@ -0,0 +1,11 @@
+#PYDROID
+import time
+id = "save/lol"
+print("Type something.")
+x = input()
+print("Wait 10 seconds and the program starts.")
+time.sleep(9)
+print("Started!")
+while True:
+    print(x)
+    time.sleep(0.1)
