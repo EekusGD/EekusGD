0b57dae0b320e33a5241e97e5c45ae101f525395
EekusGD <andrewche321@gmail.com>
2024-09-22 16:21:03 +0300

    Add files via upload
    wrong a$$

---
+++ Impossible.py
@@ -0,0 +1,14 @@
+#PYDROID
+id = "save/Impossible"
+while True:
+    print("484837373485869695 + 759595959595959595 = ")
+    z = input().lower()
+    x = str(484837373485869695 + 759595959595959595)
+    if z == x:
+        print("Right!")
+        break
+        print("The answer is " + x)
+    else:
+        print("wrong ass")
+        print("")
+    
